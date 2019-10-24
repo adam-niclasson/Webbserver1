ecba298620596c992fa9b6f5a7bb9ece8a240a1c
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-10-24 10:01:35 +0200

    zxc

--- wesweb/firstprogram/server.js
+++ wesweb/firstprogram/server.js
@@ -11,7 +11,7 @@ switch (request.url) {
     case "/":
         return sendFile(response, 200, "text/html", "./client/flexboxserver.html");
     case "/Gopnik.jpg":
-        return sendFile(response, 200, "image/jpeg","./client/Gopnik.jpg");
+        return sendFile(response, 200, "image/jpg","./client/Gopnik.jpg");
     case "/flexboxserver.css":
         return sendFile(response, 200, "text/css", "./client/flexboxserver.css");
 
