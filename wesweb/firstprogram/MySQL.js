eac07079667142e5bc7334ff44ad8ff01e9d3e47
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-10-24 11:26:22 +0200

    asd

---
+++ wesweb/firstprogram/MySQL.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+});
+
+con.connect(function(err) {
+if
+});
