78eaf68028a2968a8268f9c2295f59c2df8ba117
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-11-07 10:47:37 +0100

    yeet

---
+++ wesweb/firstprogram/MongoDB.js
@@ -0,0 +1,10 @@
+var mongo = require('mongodb');
+
+var MongoClient = requre('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
