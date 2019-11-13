394da3a1b15baf60a2c373043c02c6a6d31c3ef5
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-11-13 14:38:01 +0100

    asd

--- wesweb/firstprogram/MongoDB.js
+++ wesweb/firstprogram/MongoDB.js
@@ -1,14 +1,12 @@
-var mongo = require('mongodb');
-
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.collection("customers").findOne({}, function(err, result) {
+        dbo.collection("customers").drop(function(err, delOK) {
         if (err) throw err;
-        console.log(result.name);
+        if (delOK) console.log("Collection deleted");
         db.close();
     });
 });
