90f0bf2a75c300b9c6d6973620452ed2acc29ab0
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-11-20 14:44:49 +0100

    yeet

--- wesweb/firstprogram/myModule.js
+++ wesweb/firstprogram/myModule.js
@@ -1,6 +1,32 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-exports.firstFunction = function (a, b){
-console.log(a+b)
-}
+exports.firstFunction = function (email, name, message)
+{
+    console.log("test")
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        var query = { email: `${email}` };
+        dbo.collection("WESWEB").find(query).toArray(function (err, result) {
+            if (err) throw err;
+            console.log("DEBUG")
+            console.log(result);
+            if (result.length!=0) {
+                var newvalues = { $set: { name: `${name}`, message: `${message}` } };
+                dbo.collection("WESWEB").updateOne(query, newvalues, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document updated");
+                    db.close();
+                });
+            } else {
+                var myobj = { name: `${name}`, email: `${email}`, messege: `${messege}` };
+                dbo.collection("WESWEB").insertOne(myobj, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                });
+            }
+        });
+    });
+};
