8f178da8b88fafb607729c3d07b8e3c589dc9648
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-11-21 10:29:09 +0100

    asd

--- wesweb/firstprogram/MongoDB.js
+++ wesweb/firstprogram/MongoDB.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-        dbo.collection("customers").drop(function(err, delOK) {
+        dbo.dropCollection("customers", function(err, delOK) {
         if (err) throw err;
         if (delOK) console.log("Collection deleted");
         db.close();
