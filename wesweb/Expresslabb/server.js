9f7c97d498b6647c0e430b887c06f1758f8a5c81
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-12-04 09:43:06 +0100

    asd

--- wesweb/Expresslabb/server.js
+++ wesweb/Expresslabb/server.js
@@ -1,38 +1,22 @@
 const express = require('express');
 const app = express();
-const port = (3030);
-const clientDir = (__dirname + '\\client\\');
-
-const sendFile = (response, status, type, filePath) => {
-    response.writeHead(status, {"Content-Type": type });
-    createReadStream(filePath).pipe(response);
-};
-
-createServer((request, response) => {
-  if (request.method === "POST") {
-      let body = "";
-      request.on("data", data => {
-        body += data;
-      });
-      request.on("end", () => {
-        const { Username, Password } = decode(body);
-        firstFunction(Username, Password);
-        console.log(`email: ${Username}, name: ${Password}`);
-      });        
-      return sendFile(response, 200, "text/html", "./client/indexogin.html");
-    };
-
-
-
-
+const port = 3030;
+const clientDir = __dirname + '\\client\\';
 
 app.get('/', (req, res) => {
-  res.sendFile(clientDir+'index.html'+'index.css')
-});
-
+  res.sendFile(clientDir + 'index.html')
+})
+app.get('/index.css', (req, res) => {
+  res.sendFile(clientDir + 'index.css')
+})
+app.get('/Waterfall.gif', (req, res) => {
+  res.sendFile(clientDir + 'Waterfall.gif')
+})
 app.get('/indexlogin.html', (req, res) => {
-  res.sendFile(clientDir+'/indexlogin.html'+'indexlogin.css')
-});
-
-app.listen(port, () => console.log(`Listening to port 3030!`));
-});
+  res.sendFile(clientDir + 'indexlogin.html')
+})
+app.get('/indexlogin.css', (req, res) => {
+  res.sendFile(clientDir + 'indexlogin.css')
+})
+app.listen(port, () => console.log(`Listening to port 3030!`)
+);
