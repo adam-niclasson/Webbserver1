0b7fa84032aff52b07038865d79a47080d8f8fcd
adam-niclasson <adam-niclasson@users.noreply.github.com>
2019-12-19 10:28:32 +0100

    asd

--- wesweb/Expresslabb/server.js
+++ wesweb/Expresslabb/server.js
@@ -35,13 +35,13 @@ app.post('/users', async (req, res) => {
 app.post('/users/login', async (req, res) => {
   const user = users.find(user => user.name === req.body.name)
   if (user == null) {
-    return. res.status(400).send('Cannot find user')
+    return res.status(400).send('Cannot find user')
   }
   try{
-    if(await bcrypt.compare(req.body.password)) {
+    if(await bcrypt.compare(req.body.password, user.password)) {
       res.send('Success')
     } else {
-
+        res.send('Not Allowed')
     }
   } catch {
     
