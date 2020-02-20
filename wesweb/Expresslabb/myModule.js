ab9a7ea170b1d9202c161bdf664c046f5c0cd35c
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-02-20 10:39:39 +0100

    yes

--- wesweb/Expresslabb/myModule.js
+++ wesweb/Expresslabb/myModule.js
@@ -1,34 +1,35 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
 
-exports.firstFunction = function (Username, Password)
-{
-    console.log("test")
-    MongoClient.connect(url, function (err, db) {
+exports.firstFunction = function(Username, Password) {
+  console.log("test");
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    var query = { Username: `${Username}`, Password: `${Password}` };
+    dbo
+      .collection("WESWEB")
+      .find(query)
+      .toArray(function(err, result) {
         if (err) throw err;
-        var dbo = db.db("test");
-        var query = { Username: `${Username}`, Password:`${Password}`  };
-        dbo.collection("WESWEB").find(query).toArray(function (err, result) {
+        console.log("DEBUG");
+        console.log(result);
+        if (result.length != 0) {
+          console.log("Person allready in db");
+          db.close();
+        } else {
+          var myobj = { Username: `${Username}`, Password: `${Password}` };
+          dbo.collection("WESWEB").insertOne(myobj, function(err, res) {
             if (err) throw err;
-            console.log("DEBUG")
-            console.log(result);
-            if (result.length!=0) {
-                console.log("Person allready in db");
-                db.close();
-            } else {
-                var myobj = { Username: `${Username}`, Password: `${Password}` };
-                dbo.collection("WESWEB").insertOne(myobj, function (err, res) {
-                    if (err) throw err;
-                    console.log("1 document inserted");
-                    db.close();
-                });
-            }
-        });
-    });
+            console.log("1 document inserted");
+            db.close();
+          });
+        }
+      });
+  });
 };
 
-exports.regFunction = function (url)
-{
-    console.log("note")
-    location.href("indexreg.html")
+exports.regFunction = function(url) {
+  console.log("note");
+  location.href("indexreg.html");
 };
--- wesweb/Expresslabb/server.js
+++ wesweb/Expresslabb/server.js
@@ -1,82 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3030;
-const clientDir = __dirname + '\\client\\';
-const bodyparser = require('body-parser');
-const Module = require('./myModule');
-const bcrypt = require('bcryptjs');
+const clientDir = __dirname + "\\client\\";
+const bodyparser = require("body-parser");
+const Module = require("./myModule");
+const bcrypt = require("bcryptjs");
 
-app.use(bodyparser.urlencoded())
-app.use(express.json())
+app.use(bodyparser.urlencoded());
+app.use(express.json());
 
 const users = [];
 
-app.post('/', (req, res) => {
-  let name = req.body.Username
-  let password = req.body.Password
-  Module.firstFunction(name, password)
-  res.sendFile(clientDir + 'indexlogin.html')
-})
+app.post("/", (req, res) => {
+  let name = req.body.Username;
+  let password = req.body.Password;
+  Module.firstFunction(name, password);
+  res.sendFile(clientDir + "indexlogin.html");
+});
 
-app.post('/reg', (req, res) => {
-  Module.regFunction(url)
-  res.sendFile(clientDir + 'indexreg.html')
-})
+app.post("/reg", (req, res) => {
+  Module.regFunction(url);
+  res.sendFile(clientDir + "indexreg.html");
+});
 
-app.get('/users', (req, res) => {
-  res.json(users)
-})
-app.post('/users', async (req, res) => {
-    try{
-      const hashedPassowrd = await bcrypt.hash(req.body.password, 10)
-      const user = { name: req.body.name, password: req.body.password }
-      users.push(user)
-      res.status(201).send()
-    } catch {
-      res.status(500).send()
-    }
-})
-
-app.post('/users/login', async (req, res) => {
-  const user = users.find(user => user.name === req.body.name)
-  if (user == null) {
-    return res.status(400).send('Cannot find user')
-  }
-  try{
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-        res.send('Not Allowed')
-    }
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassowrd = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: req.body.password };
+    users.push(user);
+    res.status(201).send();
   } catch {
-    
+    res.status(500).send();
   }
-})
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(clientDir + 'index.html')
-})
-app.get('/index.css', (req, res) => {
-  res.sendFile(clientDir + 'index.css')
-})
-app.get('/Waterfall.gif', (req, res) => {
-  res.sendFile(clientDir + 'Waterfall.gif')
-})
-app.get('/Gopnik.jpg', (req, res) => {
-  res.sendfile(clientDir + 'Gopnik.jpg')
-})
-app.get('/indexlogin.html', (req, res) => {
-  res.sendFile(clientDir + 'indexlogin.html')
-})
-app.get('/indexlogin.css', (req, res) => {
-  res.sendFile(clientDir + 'indexlogin.css')
-})
-app.get('/indexreg.html', (req, res) => {
-  res.sendFile(clientDir + 'indexreg.html')
-})
-app.get('indexreg.css', (req, res) => {
-  res.sendFile(clientDir + 'indexreg.css')
-})
+app.post("/users/login", async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {}
+});
 
-app.listen(port, () => console.log(`Listening to port 3030!`)
-);
+app.get("/", (req, res) => {
+  res.sendFile(clientDir + "index.html");
+});
+app.get("/index.css", (req, res) => {
+  res.sendFile(clientDir + "index.css");
+});
+app.get("/Waterfall.gif", (req, res) => {
+  res.sendFile(clientDir + "Waterfall.gif");
+});
+app.get("/Gopnik.jpg", (req, res) => {
+  res.sendfile(clientDir + "Gopnik.jpg");
+});
+app.get("/indexlogin.html", (req, res) => {
+  res.sendFile(clientDir + "indexlogin.html");
+});
+app.get("/indexlogin.css", (req, res) => {
+  res.sendFile(clientDir + "indexlogin.css");
+});
+app.get("/indexreg.html", (req, res) => {
+  res.sendFile(clientDir + "indexreg.html");
+});
+app.get("indexreg.css", (req, res) => {
+  res.sendFile(clientDir + "indexreg.css");
+});
+
+app.listen(port, () => console.log(`Listening to port 3030!`));
