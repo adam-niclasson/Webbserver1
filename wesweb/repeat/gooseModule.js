0215fdb2065baf4ba8b6f27e2d6ab94e10f638f1
adam-niclasson <54893668+adam-niclasson@users.noreply.github.com>
2020-03-26 11:15:56 +0100

    yes

---
+++ wesweb/repeat/gooseModule.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    //open.
+});
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+}
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
